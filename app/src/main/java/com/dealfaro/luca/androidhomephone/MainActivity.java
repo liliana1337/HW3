1d765e665131b140ce68a228679e8a148fc7a870
Luca de Alfaro <luca@dealfaro.org>
2017-04-17 23:00:24 -0700

    The simple request works

--- app/src/main/java/com/dealfaro/luca/androidhomephone/MainActivity.java
+++ app/src/main/java/com/dealfaro/luca/androidhomephone/MainActivity.java
@@ -2,8 +2,13 @@ package com.dealfaro.luca.androidhomephone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,12 +23,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        // We need the "final" keyword here to guarantee that the
+        // value does not change, as it is used in the callbacks.
+        final TextView mTextView = (TextView) findViewById(R.id.my_text);
+
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
+        String url ="https://luca-ucsc-teaching-backend.appspot.com/api/get_list";
 
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        mTextView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
     }
-
 
 }
