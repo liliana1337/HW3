c207384c1fb3dc13511de7e73d5c5918c307e4b9
Liliana <liliana1337@gmail.com>
2017-04-26 22:58:09 -0700

    buttons

--- app/src/main/java/com/dealfaro/luca/androidhomephone/MainActivity.java
+++ app/src/main/java/com/dealfaro/luca/androidhomephone/MainActivity.java
@@ -1,12 +1,12 @@
 package com.dealfaro.luca.androidhomephone;
 
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -18,146 +18,70 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
-import static com.dealfaro.luca.androidhomephone.R.id.detailView;
-
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnPost;
+    private Button btnGet;
     private final String LOG_TAG = "androidhomephone";
-
     RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPost=(Button)findViewById(R.id.buttonPost);
+        btnGet=(Button)findViewById(R.id.buttonGet);
+        //mTextView = (TextView) findViewById(R.id.my_text);
         queue = Volley.newRequestQueue(this);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // We need the "final" keyword here to guarantee that the
-        // value does not change, as it is used in the callbacks.
-
-        getList();
-        sendMsg("Anna", "Clara", "Tschuss!");
-        getMessages("mario");
-    }
-
-    private void sendMsg(final String sender, final String recipient, final String msg) {
-
-        StringRequest sr = new StringRequest(Request.Method.POST,
-                "https://luca-ucsc-teaching-backend.appspot.com/api_w_ndb/send_msg",
-                new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d(LOG_TAG, "Got:" + response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-        }){
-            @Override
-            protected Map<String,String> getParams(){
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("sender", sender);
-                params.put("recipient" , recipient);
-                params.put("msg", msg);
-                return params;
-            }
-
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("Content-Type","application/x-www-form-urlencoded");
-                return params;
-            }
-        };
-        queue.add(sr);
-
     }
 
 
-    private void getMessages(final String recipient) {
-
-        // Instantiate the RequestQueue.
-        String url = "https://luca-ucsc-teaching-backend.appspot.com/api_w_ndb/get_msg_for_me";
-
-        String my_url = url + "?recipient=" + URLEncoder.encode(recipient);
-
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d(LOG_TAG, "Received: " + response.toString());
-                        // Ok, let's disassemble a bit the json object.
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        Log.d(LOG_TAG, error.toString());
-                    }
-                });
-
-        // In some cases, we don't want to cache the request.
-        // jsObjRequest.setShouldCache(false);
-
-        queue.add(jsObjRequest);
-    }
-
-
-
-    private void getList() {
+    public void HandleClick(View view) {
+        String url;
         final TextView mTextView = (TextView) findViewById(R.id.my_text);
-        final TextView detailView = (TextView) findViewById(R.id.detailView);
+        switch (view.getId()) {
+            case R.id.buttonGet:
+                // do get
+                url ="https://luca-ucsc-teaching-backend.appspot.com/hw3/request_via_get?token=abracadabra";
+                JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-        // Instantiate the RequestQueue.
-        String url = "https://luca-ucsc-teaching-backend.appspot.com/api/get_list";
-
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        mTextView.setText("Response json: " + response.toString());
-                        Log.d(LOG_TAG, "Received: " + response.toString());
-                        // Ok, let's disassemble a bit the json object.
-                        try {
-                            JSONArray receivedList = response.getJSONArray("mylist");
-                            String allTogether = "(";
-                            for (int i = 0; i < receivedList.length(); i++) {
-                                allTogether += receivedList.getString(i) + ";";
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                mTextView.setText("Response: " + response.toString());
                             }
-                            allTogether += ")";
-                            detailView.setText(allTogether);
-                        } catch (Exception e) {
-                            mTextView.setText("Aaauuugh, received bad json: " + e.getStackTrace());
-                        }
-                    }
-                }, new Response.ErrorListener() {
+                        }, new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        Log.d(LOG_TAG, error.toString());
-                    }
-                });
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO Auto-generated method stub
+                                Log.d(LOG_TAG, error.toString());
+                            }
+                        });
+                queue.add(jsObjRequest);
+                break;
+            case R.id.buttonPost:
+                // do post
+                url ="https://luca-ucsc-teaching-backend.appspot.com/hw3/request_via_post?token=abracadabra";
+                JsonObjectRequest jsObjRequestpost = new JsonObjectRequest
+                        (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
 
-        // In some cases, we don't want to cache the request.
-        // jsObjRequest.setShouldCache(false);
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                mTextView.setText("Response: " + response.toString());
+                            }
+                        }, new Response.ErrorListener() {
 
-        queue.add(jsObjRequest);
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO Auto-generated method stub
+                                Log.d(LOG_TAG, error.toString());
+                            }
+                        });
+                queue.add(jsObjRequestpost);
+                break;
+        }
+
     }
 
 }
+
